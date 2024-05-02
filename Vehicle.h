d224bfacb09a824c3d1ac2299e3707fc40a14a16
Tomasz Jaskula <thomasv1000@hotmail.fr>
2024-05-02 10:58:39 +0000

    Move implementations out of Vehicle.h

--- Vehicle.h
+++ Vehicle.h
@@ -7,24 +7,15 @@ struct Vehicle
 {
     friend struct HighwayPatrol;
 
-    Vehicle(const std::string& n) : name(n) { }
+    Vehicle(const std::string& n);
     
     //Special member Functions.  See instruction 9) and note in main()
     virtual ~Vehicle() = default;
     Vehicle(const Vehicle&) = default;
     Vehicle& operator=(const Vehicle&) = default;
     
-    virtual void setSpeed(int s)
-    {
-        std::cout << "\nHighway is adjusting " << name << "'s speed to " << s << " mph" << std::endl;
-        speed = s;
-    }
-    
-    virtual void tryToEvade()
-    {
-        std::cout << name << ": you'll never take me alive, ya dirty coppers!" << std::endl;
-        setSpeed(100);
-    }
+    virtual void setSpeed(int s);
+    virtual void tryToEvade();
 protected:
     int speed = 0;
     std::string name;
